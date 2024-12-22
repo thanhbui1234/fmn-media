f0be5900be3f85cfe9479815e03cc27c202b7ece
Bùi Chí Thanh <93216434+thanhbui1234@users.noreply.github.com>
2024-12-22 12:47:27 +0700

    video banner

--- src/components/ui/banner.tsx
+++ src/components/ui/banner.tsx
@@ -12,21 +12,14 @@ const Banner: React.FC = () => {
         loop
         muted
         className="absolute top-0 left-0 h-full w-full object-cover"
+        onError={(e) => {
+          e.currentTarget.style.display = 'none';
+          e.currentTarget.parentElement!.style.backgroundColor = 'black';
+        }}
       >
-        <source
-          src="/assets/iLoveYt.net_YouTube_WREN-EVANS-To-Te-Ti-LOI-CHOI-The-First-A_Media_aCqmdPaTVSg_002_720p.mp4"
-          type="video/mp4"
-        />
+        <source src="/assets/defaultBanner.mp4" type="video/mp4" />
       </video>
-      {/* <ReactPlayer
-        url="/assets/iLoveYt.net_YouTube_WREN-EVANS-To-Te-Ti-LOI-CHOI-The-First-A_Media_aCqmdPaTVSg_002_720p.mp4"
-        playing={true}
-        loop={true}
-        muted={true}
-        // width="100%"
-        // height="100%"
-        className="absolute top-0 left-0 h-full w-full object-cover"
-      /> */}
+
       <div className="absolute inset-0 flex items-center justify-center bg-black/50">
         <h1 className="text-4xl md:text-6xl text-white font-bold text-center">
           Chào Mừng Bạn Đến Với Shadcn UI
